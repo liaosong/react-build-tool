5790b73831aa4bbd66c1b1229d3f440595d286af
Think <liaosong1015@gmail.com>
2015-11-13 11:15:22 +0800

    init reps

---
+++ gulpfile.babel.js
@@ -0,0 +1,14 @@
+import gulp from 'gulp';
+import webpack from 'webpack';
+
+
+
+//for production build
+gulp.task("build", function(){
+
+});
+
+//for devlopment build
+gulp.task("serve", function(){
+
+});
