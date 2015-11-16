7a1298bcb8ee6078b8226dac3907865d649f836f
Think <liaosong1015@gmail.com>
2015-11-16 16:57:00 +0800

    react build tool for server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,21 +2,46 @@ import gulp from 'gulp';
 import webpack from 'webpack';
 import gulpUtil from 'gulp-util';
 import path from 'path';
+import jade from 'gulp-jade';
+import sass from 'gulp-sass';
 
+const PATHS = {
+  src: 'app',
+  html: ['app/*.html', 'app/*/*.html'],
+  dist: 'dist/',
+  js: ['app/*.js', 'app/*/*.js'],
+  all_sass: ['app/styles/*.scss', 'app/styles/*/*.scss'], //scss watch的文件
+  module_sass: 'app/styles/*.scss', //scss相应的模块文件
+  module_js: {app:'./app/app.js', index:'./app/inde.js'}, //页面对应的js文件
 
+};
 
+gulp.task('sass', function(){
+  gulp.src(PATHS.module_sass, {base: PATHS.src})
+    .pipe(sass())
+    .pipe(gulp.dest(PATHS.dist));
+});
+gulp.task("copy", function(){
+  gulp.src(PATHS.html)
+    .pipe(gulp.dest(PATHS.dist));
+});
 //for production build
 gulp.task("build", function(){
 
 });
 
 //for devlopment build
-gulp.task("serve", function(){
+gulp.task("serve",[
+  'sass',
+  'webpack',
+  'watch'
+]);
+gulp.task("webpack", ['copy'], function(){
   webpack({
-    entry: "./app/app.js",
+    entry: PATHS.module_js,
     output: {
       path: path.join(__dirname, '/dist'),
-      filename: "app.js"
+      filename: '[name].js'
     },
     resolve: {
       extensions: ['', '.js', '.jsx']
@@ -28,8 +53,12 @@ gulp.task("serve", function(){
         }
       ]
     }
-  }, function(err, status){
+  }, function(err){
     if(err) throw new gulpUtil.PluginError("webpack", err);
-    // gulpUtil.log("[webpack]", status.toString());
   });
+});
+
+gulp.task('watch', function(){
+  gulp.watch(PATHS.js, ['webpack']);
+  gulp.watch(PATHS.all_sass, ['sass']);
 });
