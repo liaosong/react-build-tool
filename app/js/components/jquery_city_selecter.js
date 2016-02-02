f06fef2b0bc9acb84d06a5fefc33cd09def2070e
Think <liaosong1015@gmail.com>
2016-02-02 10:23:39 +0800

    index

--- app/js/components/jquery_city_selecter.js
+++ app/js/components/jquery_city_selecter.js
@@ -1,20 +1,46 @@
-import $ from 'jquery';
+import {cities} from '../global_data'
 
 export default function citySelecter(){
+    var cityContainer = this.find('.city-container');
+    var self = this;
 
-    //$(".city-selecter").click(function(){
-    //    var cityContainer = $(this).find('.city-container');
-    //    function show(){
-    //        cityContainer.addClass('show');
-    //    }
-    //    function hidden(){
-    //        cityContainer.removeClass('show');
-    //    }
-    //
-    //    cityContainer.find("li").click(function(e){
-    //        e.stopPropagation();
-    //        hidden();
-    //    })
-    //    show();
-    //});
+    function show(){
+        cityContainer.addClass('show');
+    }
+    function hidden(){
+        cityContainer.removeClass('show');
+    }
+
+    function init(){
+        var defaultVal = self.find(".value").val();
+        var clitiesHtml = cities.map(function(item){
+            if(defaultVal === item){
+                return `<li class="selected">${item}</li>`;
+            }else{
+                return `<li>${item}</li>`;
+            }
+
+        });
+        cityContainer.html(clitiesHtml.join(''));
+
+    }
+
+    function setSelected(val){
+        self.find(".value").val(val);
+    }
+
+    this.click(function(e){
+        var isOpen = cityContainer.hasClass('show');
+        if(e.target.nodeName === 'LI'){
+            setSelected(e.target.innerHTML);
+            hidden(e);
+        }
+        else if(isOpen){
+            e.stopPropagation();
+        }else{
+            init();
+            show();
+        }
+
+    });
 }
--- app/js/index.js
+++ app/js/index.js
@@ -1,33 +1,19 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import thunk from 'redux-thunk';
-import { compose, createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import IndexReducers from './reducers/index';
-import Index from './module/index';
-import { devTools, persistState } from 'redux-devtools';
+import $ from 'jquery';
 
-//let store = createStore(IndexReducers);
-const finalCreateStore = compose(
-    // Enables your middleware:
-    applyMiddleware(thunk), // any Redux middleware, e.g. redux-thunk
-    // Provides support for DevTools:
-    devTools()
-    // Lets you write ?debug_session=<name> in address bar to persist debug sessions
-    //persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-)(createStore);
+import citySelecter from '../js/components/jquery_city_selecter';
 
-const store = finalCreateStore(IndexReducers);
+import dialog from 'jquery-ui/dialog';
 
-var currentUser = undefined;
+$.fn.citySelecter = citySelecter;
 
-if(window.CURRENT_USER) {
-    currentUser = window.CURRENT_USER;
-}
-let rootElement = document.getElementById('container');
-ReactDom.render(
-    <Provider store={store} >
-      <Index initUser={currentUser}/>
-    </Provider>,
-    rootElement
-);
+$(function(){
+    $(".city-selecter").citySelecter();
+
+    $("#search").click(function(e){
+        var inputVal = $("#searchText").val();
+        var cityVal = $("#cityVal").val();
+        location.href = encodeURI(`/search?q=${inputVal || ''}&city=${cityVal || ''}`);
+    });
+
+    $("#dialog").dialog({modal: true});
+});
